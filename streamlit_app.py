e93d2471a755bc0848fdeab153a2ed4b6bedd68f
snowd877 <114664500+snowd877@users.noreply.github.com>
2022-09-29 13:57:29 +0200

    Update streamlit_app.py
    removed capitals

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-Streamlit.text('Omega 3 & Blueberry Oatmeal')
-Streamlit.text('Kale, Spinach & Rocket Smoothie')
-Streamlit.text('HArd-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
